9686bdd6f5400ede1b09e132de1ceb9bdce801be
shivamsouravjha <shivamsouravjha@gmail.com>
2024-04-09 06:55:44 +0000

    refactor:  moving jacoco download to url

--- v2/src/main/java/io/keploy/Keploy.java
+++ v2/src/main/java/io/keploy/Keploy.java
@@ -487,7 +487,6 @@ public class Keploy {
     }
 
     public static void runTests(String jarPath, RunOptions runOptions) {
-
         String runCmd = "java -jar " + jarPath;
         if (runOptions.getPort() != 0) {
             serverPort = runOptions.getPort();
